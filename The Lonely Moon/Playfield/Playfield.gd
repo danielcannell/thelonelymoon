2a11e604382dfc0b2bdd6cb961b7deb80c9962ba
Daniel Cannell <danielcannell@googlemail.com>
2018-08-11 22:06:23 +0100

    Remove uneeded resource

--- The Lonely Moon/Playfield/Playfield.gd
+++ The Lonely Moon/Playfield/Playfield.gd
@@ -3,7 +3,6 @@ extends Node2D
 signal satellite_summary
 signal satellite_selected
 
-var Satellite = preload("res://Playfield/Satellite/Satellite.tscn");
 var Debris = preload("res://Playfield/Satellite/debris/Debris.tscn");
 var SpySatellite = preload("res://Playfield/Satellite/spy_satellite/SpySatellite.tscn");
 var CubeSat = preload("res://Playfield/Satellite/cube_sat/CubeSat.tscn");
