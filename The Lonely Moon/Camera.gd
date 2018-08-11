2ad04faffe73f9158776e235d6055e76258666cc
James Buckley <xanium4332@gmail.com>
2018-08-11 17:01:14 +0100

    Switch scaling to use explicit camera

---
+++ The Lonely Moon/Camera.gd
@@ -0,0 +1,23 @@
+extends Camera2D
+
+func update_scale(ratio):
+    var new_scale = self.zoom.x * ratio
+    new_scale = min(new_scale, 8.0)
+    new_scale = max(new_scale, 0.1)
+    self.zoom.x = new_scale
+    self.zoom.y = new_scale
+    
+    # Keep the earth's position fixed relative to the centre of the viewport
+    self.offset.x = -100 * new_scale
+
+func _input(event):
+    if event is InputEventMouseButton:
+        if event.is_pressed():
+            # zoom in
+            if event.button_index == BUTTON_WHEEL_UP:
+                update_scale(1.1)
+
+            # zoom out
+            if event.button_index == BUTTON_WHEEL_DOWN:
+                update_scale(0.9)
+                
