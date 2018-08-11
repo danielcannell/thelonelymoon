0601d28150e51889e8855366464856beaa2d7963
Ben Janoff <ben.janoff@gmail.com>
2018-08-11 13:27:40 +0100

    Add ship config global

--- The Lonely Moon/Playfield/Playfield.gd
+++ The Lonely Moon/Playfield/Playfield.gd
@@ -2,40 +2,14 @@ extends Container
 
 export (PackedScene) var Satellite
 
-var spawn_config = [
-	{
-		'name': 'spy_satellite',
-	 	'alt_min': 500,
-	 	'alt_max': 520,
-	 	'delta_v': 100,
-	},
-	{
-		'name': 'science_station',
-	 	'alt_min': 600,
-	 	'alt_max': 700,
-	 	'delta_v': 150,
-	}
-]
-
 func _ready():
-	# Called when the node is added to the scene for the first time.
-	# Initialization here
 	new_craft('spy_satellite')
 	new_craft('spy_satellite')
 	new_craft('science_station')
 
 
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
-
 func new_craft(type):
-	var config
-	for t in spawn_config:
-		if t.name == type:
-			config = t
-			break
+	var config = global.ship_config(type)
 
 	var craft = Satellite.instance()
 	add_child(craft)
--- The Lonely Moon/global.gd
+++ The Lonely Moon/global.gd
@@ -1,17 +1,59 @@
 extends Node
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
-
 func _ready():
-	# Called when the node is added to the scene for the first time.
-	# Initialization here
 	pass
 
+const SHIP_CONFIG = [
+	{
+		'name': 'cube_sat',
+		'display_name': 'CubeSat',
+		'description': 'Pretend that you\'re NASA, on the cheap.',
+	 	'alt_min': 350,
+	 	'alt_max': 380,
+	 	'delta_v': 50,
+		'cost': 500,
+		'income': 10,
+	},
+	{
+		'name': 'spy_satellite',
+		'display_name': 'Spy Satellite',
+		'description': 'Keep an eye on the neighbours.',
+	 	'alt_min': 500,
+	 	'alt_max': 520,
+	 	'delta_v': 100,
+		'cost': 1000,
+		'income': 25,
+	},
+	{
+		'name': 'science_station',
+		'display_name': 'Science Station',
+		'description': 'We\'re doing science and we\'re still alive',
+	 	'alt_min': 600,
+	 	'alt_max': 700,
+	 	'delta_v': 150,
+		'cost': 10000,
+		'income': 100,
+	},
+	{
+		'name': 'space_hotel',
+		'display_name': 'Space Hotel',
+		'description': '***: Not much atmosphere, but great views.',
+	 	'alt_min': 300,
+	 	'alt_max': 600,
+	 	'delta_v': 150,
+		'cost': 100000,
+		'income': 1000,
+	}
+]
 
 const METRES_PER_SCREEN_UNIT = 1.0 / 200.0;
 
+func ship_config(name):
+	for s in SHIP_CONFIG:
+		if s.name == name:
+			return s
+	
+	return {}
 
 func screen_to_metres(vec):
 	return vec / METRES_PER_SCREEN_UNIT
